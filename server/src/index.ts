4807fed579d786040c8c48b8f91a6d0bf014737d
Jesper Hägglund <jeppan875@gmail.com>
2021-05-27 13:13:13 +0200

    connect to the movie database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,18 @@
-import express, { Application, Request, Response, NextFunction } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import fetch from 'node-fetch'
+require('dotenv').config()
 
-// Boot express
 const app: Application = express()
 const port = 3013
 
 app.use(cors())
 
-// Application routing
-app.use('/', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ data: 'Hello world' })
+app.use('/', async (req: Request, res: Response) => {
+  const popularMoviesRes = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`)
+  const popularMoviesStr = await popularMoviesRes.text()
+  const popularMovies = JSON.parse(popularMoviesStr)
+  res.status(200).send({ data: popularMovies })
 })
 
-// Start server
 app.listen(port, () => console.log(`Server is listening on port ${port}!`))
