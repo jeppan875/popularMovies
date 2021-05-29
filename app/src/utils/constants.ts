c86baf190789893ec199b43fc9dc613336a55f42
Jesper Hägglund <jeppan875@gmail.com>
2021-05-29 21:49:09 +0200

    movie database constants

---
+++ app/src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const posterSizes = {
+  '92': 92,
+  '154': 154,
+  '185': 185,
+  '342': 342,
+  '500': 500,
+  '780': 780,
+}
+
+export const posterBaseUrl = `https://image.tmdb.org/t/p/w`
